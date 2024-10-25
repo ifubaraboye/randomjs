e9be36100410285ea7064140f93f3db2ed559e4b
Oribi <ifubaraboyeoribime@outlook.com>
2024-10-25 16:38:43 +0100

    added file

---
+++ if.js
@@ -0,0 +1,57 @@
+// let user = prompt("Who's There")
+// let password = 'TheMaster'
+
+
+
+// if (user == 'Admin'){
+//     let userpassword = prompt('Enter Passwword');
+//     if (userpassword == password){
+//         alert('Welcome!')
+//     }else if (userpassword == null){
+//         alert('Canceled')
+//     }else alert('Wrong Password')
+// }else if (user == null){
+//     alert('Canceled')
+// }else alert("User Not Recognized")
+
+
+// let browser = prompt('Enter your browser')
+
+// if (browser == 'Edge'){
+//     alert("You've got the edge")
+// }else if (browser == 'Chrome'){
+//     alert('We Support')
+// }else if (browser == 'Safari'){
+//     alert('We support')
+// }else if (browser == 'Firefox'){
+//     alert('We Support')
+// }else if (browser == 'Opera'){
+//     alert('We support')
+// }else alert('KK')
+    
+   
+
+let a = +prompt('a?', '')
+
+switch (a){
+    case 0:
+        alert('0');
+        break;
+
+
+    case 1:
+        alert('1');
+        break;
+
+
+    case 2:
+    case 3:
+        alert('2,3')
+        break;
+
+
+    default:
+        alert('Fuck OFF')
+}
+    
+        
